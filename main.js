d44c04a84b116db64ac227ab27db4d98a9c2cad8
Kazimir84 <kav2007@ukr.net>
2021-09-11 21:15:34 +0300

    Fixed remark

--- main.js
+++ main.js
@@ -2,23 +2,30 @@ let imgList = document.getElementById('imgList')
 let dispList = document.getElementById('list');
 
 async function getAlbumList() {
-  let list = await fetch('http://jsonplaceholder.typicode.com/albums')
-  let albumList = await list.json();
-  dispList.innerHTML = albumList.map((data) => {
-    let li = `<li value="${data.id}"> Id: ${data.id} Title: ${data.title}. UserId: ${data.userId}</li>`;
-    return li;
+  let responseList = await fetch('http://jsonplaceholder.typicode.com/albums')
+  let resaultAlbumList = await responseList.json();
+  dispList.innerHTML = resaultAlbumList.map((data) => {
+    return `<li value="${data.id}"> Id: ${data.id} Title: ${data.title}. UserId: ${data.userId}</li>`;
   }).join('');
 };
-getAlbumList().then(() => getLogo(dispList.value));
+// getAlbumList().then(() => getLogo());
+getAlbumList();
 
 async function getLogo(id = 1) {
-  let logo = await fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`);
-  let logos = await logo.json();
-  imgList.innerHTML = logos.map((data) => {
-    let img = `<img src="${data.url}">`
-    return img;
-  }).join('');
+  let responseLogo = await fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`);
+  let resaultLogos = await responseLogo.json();  
+  // imgList.innerHTML = resaultLogos.map((data) => {
+  //   let img = `<img src="${data.url}">`
+  //   return img;
+  // }).join('');  
+  let out = '<div>';
+  for( let i in resaultLogos ) {
+    out += `<li class="logo"><img src='${resaultLogos[i].url}'</li>`
+  }
+  out += '</div>'
+  imgList.innerHTML = out;
 };
+getLogo();
 
 dispList.addEventListener('click', elem => {
   let resault = elem.target.value;
